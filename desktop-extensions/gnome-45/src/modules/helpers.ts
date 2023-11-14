71ee9e43bac488051eea3bcc8943b7a0f0b0f50f
Luke D. Jones <luke@ljones.dev>
2023-11-14 16:48:11 +1300

    Fix gnome-45 extension version

--- desktop-extensions/gnome-45/src/modules/helpers.ts
+++ desktop-extensions/gnome-45/src/modules/helpers.ts
@@ -1,11 +1,9 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import {QuickSettingsMenu, QuickToggle } from 'resource:///org/gnome/shell/ui/quickSettings.js';
+import { QuickToggle } from 'resource:///org/gnome/shell/ui/quickSettings.js';
 
 export function addQuickSettingsItems(items: [typeof QuickToggle], width = 1) {
     const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
-    // Add the items with the built-in function
-    QuickSettingsMenu._addItems(items, width);
-
+    items.forEach(item => QuickSettingsMenu.menu.addItem(item, width));
     // Ensure the tile(s) are above the background apps menu
     for (const item of items) {
         QuickSettingsMenu.menu._grid.set_child_below_sibling(item,
